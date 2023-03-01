4ad65915c5082a39e28ec9750409609f090032ef
cbolles <collinbolles@gmail.com>
2023-03-01 14:59:52 -0500

    Get mongo uri from config

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,16 +4,27 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { OrganizationModule } from './organization/organization.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './config/configuration';
 
 
 @Module({
   imports: [
     AuthModule,
+    ConfigModule.forRoot({
+      load: [configuration]
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
       driver: ApolloDriver
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nist'),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('mongo.uri')
+      }),
+      inject: [ConfigService]
+    }),
     OrganizationModule
   ]
 })
