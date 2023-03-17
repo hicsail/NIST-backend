b97d9b2a18925b4618692aaa22f344f40725ef0f
cbolles <collinbolles@gmail.com>
2023-03-17 12:03:54 -0400

    Setup NestJS project for Apollo Gateway

--- packages/federation/src/main.ts
+++ packages/federation/src/main.ts
@@ -1,1 +1,8 @@
-console.log('hi');
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+bootstrap();
